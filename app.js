7798f854098f70182f08fa6dda4c8b22e00c8eff
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-10-06 11:14:35 +0300

    Trying to fix heroku deployment

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
+const server = jsonsServer.create()
+const router = jesonServer.router('')
+const middlewares = jsonsServer.defaults()
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+server.use('/api', middlewares, router)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
 
-app.get('/health', (req, res) => {
+server.get('/health', (req, res) => {
   res.send('ok')
 })
+
+server.use(jsonsServer.bodyParser)
+server.use((req, res, next) => {
+  next()
+})
+
+server.use(app.use(express.static('dist')))
